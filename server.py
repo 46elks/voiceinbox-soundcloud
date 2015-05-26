9477d019c2bd7debc7436440d24dad5002e671a4
Martin Harari Thuresson <martin@46elks.com>
2015-05-26 15:25:32 +0200

    cleaned exceptions

--- server.py
+++ server.py
@@ -10,50 +10,49 @@ import local
 
 # Handle incoming event:
 def application(env, start_response):
-    
-    # read POST data:
+
     try:
-        data = urlparse.parse_qs(env["wsgi.input"].read());
-    except:
-        start_response('500 Internal Server Error',[('content-type', 'text/html')])
-        return 'POST request data error.'     
+        # read POST data:
+        try:
+            data = urlparse.parse_qs(env["wsgi.input"].read());
+        except:
+            raise Exception('POST request data error.')
+   
+        # GET sound File:
+        try:
+            r = requests.get(data["wav"][0], auth=(local.user46elks, local.pass46elks))
+            file = open('soundfiles/'+data["callid"][0]+'.wav', "w")
+            file.write(r.content)
+            file.close()
+        except:
+            raise Exception('Retrieving file from sender failed.')
     
-    # GET sound File:
-    try:
-        r = requests.get(data["wav"][0], auth=(local.user46elks, local.pass46elks))
-        file = open('soundfiles/'+data["callid"][0]+'.wav', "w")
-        file.write(r.content)
-        file.close()
-        
-    except:
-        start_response('500 Internal Server Error',[('content-type', 'text/html')])
-        return 'Retrieving file from sender failed.'
+        # Authenticate with Soundcloud
+        try:
+            client = soundcloud.Client(
+                client_id=local.idsoundcloud,
+                client_secret=local.secretsoundcloud,
+                username=local.usersoundcloud,
+                password=local.passsoundcloud
+                )
+        except:
+            raise Exception('Unable to authenticate with soundcloud.')
     
-    # Authenticate with Soundcloud
-    try:
-        client = soundcloud.Client(
-            client_id=local.idsoundcloud,
-            client_secret=local.secretsoundcloud,
-            username=local.usersoundcloud,
-            password=local.passsoundcloud
-            )
-            
-    except:
-        start_response('500 Internal Server Error',[('content-type', 'text/html')])
-        return 'Unable to authenticate with soundcloud.'
+        # POST file to Soundcloud
+        try:
+            track = client.post(
+                '/tracks', 
+                track={
+                    'title': 'Voice recording from: '+data["from"][0],
+                    'asset_data': open('soundfiles/'+data["callid"][0]+'.wav', 'rb')
+                    }
+                )
+        except:
+            raise Exception('Unable to upload to soundcloud.')
     
-    # POST file to Soundcloud
-    try:
-        track = client.post(
-            '/tracks', 
-            track={
-                'title': 'Voice recording from: '+data["from"][0],
-                'asset_data': open('soundfiles/'+data["callid"][0]+'.wav', 'rb')
-                }
-            )
-    except:
+    except Exception as e:
         start_response('500 Internal Server Error',[('content-type', 'text/html')])
-        return 'Unable to upload to soundcloud.'
+        return e
     
     #success
     start_response('200 OK', [('Content-Type', 'application/json')])
